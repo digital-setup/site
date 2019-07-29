bdbb5ac575670810ae21fe01e9aba17433d675be
Fabio Quintana <fabio@hellice.com>
2019-07-29 03:46:02 -0300

    ativação do form

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+var cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(cors());
+
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,99 +1,163 @@
 $(document).ready(function() {
+  /***************** Waypoints ******************/
 
-	/***************** Waypoints ******************/
+  $(".wp1").waypoint(
+    function() {
+      $(".wp1").addClass("animated fadeInLeft");
+    },
+    {
+      offset: "75%"
+    }
+  );
+  $(".wp2").waypoint(
+    function() {
+      $(".wp2").addClass("animated fadeInDown");
+    },
+    {
+      offset: "75%"
+    }
+  );
+  $(".wp3").waypoint(
+    function() {
+      $(".wp3").addClass("animated bounceInDown");
+    },
+    {
+      offset: "75%"
+    }
+  );
+  $(".wp4").waypoint(
+    function() {
+      $(".wp4").addClass("animated fadeInDown");
+    },
+    {
+      offset: "75%"
+    }
+  );
 
-	$('.wp1').waypoint(function() {
-		$('.wp1').addClass('animated fadeInLeft');
-	}, {
-		offset: '75%'
-	});
-	$('.wp2').waypoint(function() {
-		$('.wp2').addClass('animated fadeInDown');
-	}, {
-		offset: '75%'
-	});
-	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated bounceInDown');
-	}, {
-		offset: '75%'
-	});
-	$('.wp4').waypoint(function() {
-		$('.wp4').addClass('animated fadeInDown');
-	}, {
-		offset: '75%'
-	});
+  /***************** Flickity ******************/
 
-	/***************** Flickity ******************/
+  $("#featuresSlider").flickity({
+    cellAlign: "left",
+    contain: true,
+    prevNextButtons: false
+  });
 
-	$('#featuresSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false
-	});
+  $("#showcaseSlider").flickity({
+    cellAlign: "left",
+    contain: true,
+    prevNextButtons: false,
+    imagesLoaded: true
+  });
 
-	$('#showcaseSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false,
-		imagesLoaded: true
-	});
+  /***************** Fancybox ******************/
 
-	/***************** Fancybox ******************/
-
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
-
+  $(".youtube-media").on("click", function(e) {
+    var jWindow = $(window).width();
+    if (jWindow <= 768) {
+      return;
+    }
+    $.fancybox({
+      href: this.href,
+      padding: 4,
+      type: "iframe",
+      href: this.href.replace(new RegExp("watch\\?v=", "i"), "v/")
+    });
+    return false;
+  });
 });
 
 $(document).ready(function() {
-	$("a.single_image").fancybox({
-		padding: 4,
-	});
+  $("a.single_image").fancybox({
+    padding: 4
+  });
 });
 
 /***************** Nav Transformicon ******************/
 
 /* When user clicks the Icon */
 $(".nav-toggle").click(function() {
-	$(this).toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
+  $(this).toggleClass("active");
+  $(".overlay-boxify").toggleClass("open");
 });
 
 /* When user clicks a link */
 $(".overlay ul li a").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
+  $(".nav-toggle").toggleClass("active");
+  $(".overlay-boxify").toggleClass("open");
 });
 
 /* When user clicks outside */
 $(".overlay").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
+  $(".nav-toggle").toggleClass("active");
+  $(".overlay-boxify").toggleClass("open");
 });
 
 /***************** Smooth Scrolling ******************/
 
-$('a[href*=#]:not([href=#])').click(function() {
-	if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top
-			}, 2000);
-			return false;
-		}
-	}
+$("a[href*=#]:not([href=#])").click(function() {
+  if (
+    location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
+    location.hostname === this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      $("html,body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        2000
+      );
+      return false;
+    }
+  }
 });
+
+/*** HELLICE ***/
+//https://vnda.wufoo.com/forms/z5eilza1k3sc6u/#public
+$("#form-contact").on("submit", function(e) {
+  e.preventDefault();
+
+  if (!verifyInput($("#Field1"))) {
+    return false;
+  } else if (!verifyInput($("#Field2"))) {
+    return false;
+  } else if (!verifyInput($("#Field3"))) {
+    return false;
+  } else if (!verifyInput($("#Field4"))) {
+    return false;
+  }
+
+  $.ajax({
+    url: "https://vnda.wufoo.com/forms/z5eilza1k3sc6u/#public",
+    type: "POST",
+    data: $("#form-contact").serialize(),
+    success: function(data, textStatus) {
+      formSuccess();
+    },
+    error: function(xhr, er) {
+      formSuccess();
+    }
+  });
+
+  return true;
+});
+
+function verifyInput(div) {
+  if (!div.val()) {
+    div.addClass("error");
+    return false;
+  } else {
+    div.removeClass("error");
+  }
+  return true;
+}
+function formSuccess() {
+  $(".form-success").addClass("on");
+  setTimeout(function() {
+    $(".form-success").removeClass("on");
+    $("input, textarea").each(function() {
+      $(this).val("");
+    });
+  }, 5000);
+}
